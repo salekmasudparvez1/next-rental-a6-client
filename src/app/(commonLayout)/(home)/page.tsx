4e1a804f4f2bf879f44631381dcf52d841a4bde8
salekmasudparvez1 <smp23997@gmail.com>
2025-12-13 16:44:10 +0600

    added favicon and metadata

--- src/app/(commonLayout)/(home)/page.tsx
+++ src/app/(commonLayout)/(home)/page.tsx
@@ -1,14 +1,27 @@
 import HeroSection from '@/components/module/hero-section/HeroSection';
 import { TestimonialSection } from '@/components/module/testimonial-section/TestimonialSection';
+import { Metadata } from 'next';
 import React from 'react';
+export const metadata: Metadata = {
+  title: {
+    default: "Findbasa",
+    template: "%s - Findbasa",
+  },
+  description:
+    "Findbasa is your ultimate platform for discovering and sharing amazing content.",
+  icons: {
+    icon: "/icon.png",
+    apple: "/icon.png",
+  },
+};
 
 const Page = () => {
-    return (
-        <div>
-          <HeroSection />
-          <TestimonialSection/>
-        </div>
-    );
+  return (
+    <div>
+      <HeroSection />
+      <TestimonialSection />
+    </div>
+  );
 }
 
 export default Page;
